3db5b28e8f68e8310be22bbe3d1fce97e0abd23a
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-26 11:54:42 -0500

    Conexión con WebSockets

--- src/servidor.js
+++ src/servidor.js
@@ -4,15 +4,43 @@ import { connectDB } from "./config/connection.js";
 import { envs } from "./config/envs.js";
 import { userRouter, houseRouter } from "./routes/index.js";
 import { createHandler } from "graphql-http/lib/use/express";
+import { Socket } from "socket.io";
+import http from "node:http";
 
 const app = express();
 const SERVER_PORT = envs.PORT ?? 5001;
+const io = Socket(http.Server(app));
 
 app.disable("x-powered-by");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+
+/** Metodos websocket */
+io.on("connect", (socket) => {
+  console.log("connected");
+  //Escuchando eventos desde el servidor
+  socket.on("message", (data) => {
+    /** Almacenando el mensaje en la BD */
+    const payload = JSON.parse(data);
+    console.log(payload);
+    /** Lo almaceno en la BD */
+    MessageSchema(payload)
+      .save()
+      .then((result) => {
+        /** Enviando el mensaje a todos los clientes conectados al websocket */
+        socket.broadcast.emit("message-receipt", payload);
+      })
+      .catch((err) => {
+        console.log({ status: "error", message: err.message });
+      });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+  });
+});
 
 // TODO: Utilizar schema
 app.all("/graphql", createHandler({ schema }));
